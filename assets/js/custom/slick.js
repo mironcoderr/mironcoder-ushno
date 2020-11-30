bb0d617f09536115b7b1092981fe1eb1939824ac
mironhasan <mironmahmud018@gmail.com>
2020-11-30 11:40:26 +0600

    ushno-v1.0.01

--- assets/js/custom/slick.js
+++ assets/js/custom/slick.js
@@ -1,5 +1,44 @@
 
 
+
+// FOR TEAM SECTION SLIDER
+$('.banner-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  fade: true,
+  autoplay: true,
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: '<i class="fas fa-chevron-right dandik"></i>',
+  nextArrow: '<i class="fas fa-chevron-left bamdik"></i>',
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+});
+
+
 // FOR TEAM SECTION SLIDER
 $('.team-slider').slick({
     dots: false,
@@ -25,8 +64,6 @@ $('.team-slider').slick({
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
-          variableWidth: true,
-          arrows: false,
         }
       },
       {
@@ -119,6 +156,124 @@ $('.blog-slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         variableWidth: true,
+        arrows: false,
+      }
+    }
+  ]
+});
+  
+
+// FOR TESTIMONIAL SLIDER
+$('.testi-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  fade: false,
+  autoplay: false,
+  arrows: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: '<i class="fas fa-chevron-right dandik"></i>',
+  nextArrow: '<i class="fas fa-chevron-left bamdik"></i>',
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        arrows: false,
+      }
+    }
+  ]
+});
+  
+
+// FOR SPONSOR LOGO SLIDER
+$('.sponsor-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  fade: false,
+  autoplay: false,
+  arrows: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  prevArrow: '<i class="fas fa-chevron-right dandik"></i>',
+  nextArrow: '<i class="fas fa-chevron-left bamdik"></i>',
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+      }
+    }
+  ]
+});
+  
+
+// FOR DEMO THEME SLIDER
+$('.theme-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  fade: false,
+  autoplay: true,
+  arrows: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  prevArrow: '<i class="fas fa-chevron-right dandik"></i>',
+  nextArrow: '<i class="fas fa-chevron-left bamdik"></i>',
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
         arrows: false,
       }
     }
